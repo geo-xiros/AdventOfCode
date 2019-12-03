7886b5c536182d7b5781dd310ff01e3e1b0f6af7
George Xiros <g.xiros@microbase.gr>
2019-12-03 09:53:50 +0200

    Day2 complete

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -19,52 +19,66 @@ namespace AdventOfCode
     class Day2
     {
         private long _Answer1;
-        private long _Answer2;
-
+        private string _Answer2;
+        private Dictionary<int, Action<int, int, int>> _instructions;
+        private int[] _memory;
         public Day2()
         {
-            var program = File
-                .ReadAllText(@"C:\Users\George\source\repos\AdventOfCode\AdventOfCode\Input\input2.txt")
+            _instructions = new Dictionary<int, Action<int, int, int>>();
+            _instructions.Add(1, (parameter1, parameter2, address) => _memory[address] = _memory[parameter1] + _memory[parameter2]);
+            _instructions.Add(2, (parameter1, parameter2, address) => _memory[address] = _memory[parameter1] * _memory[parameter2]);
+
+            RunProgram(12, 2);
+
+            _Answer1 = _memory[0];
+            _Answer2 = Answer2();
+
+        }
+
+        private string Answer2()
+        {
+            for (var noun = 0; noun < 99; noun++)
+            {
+                for (var verb = 0; verb < 99; verb++)
+                {
+                    RunProgram( noun, verb);
+                    if (_memory[0] == 19690720)
+                    {
+                        return $"{noun}{verb}";
+                    }
+
+                }
+            }
+            return string.Empty;
+        }
+
+        private void RunProgram(int noun, int verb)
+        {
+            LoadFile();
+
+            _memory[1] = noun;
+            _memory[2] = verb;
+
+            for (var i = 0; i < _memory.Length; i += 4)
+            {
+                if (!_instructions.TryGetValue(_memory[i], out var instruction))
+                {
+                    return;
+                }
+
+                instruction(_memory[i + 1], _memory[i + 2], _memory[i + 3]);
+
+            }
+        }
+        private void LoadFile()
+        {
+            _memory = File
+                .ReadAllText(@"C:\Users\g.xiros\Documents\coding\adventofcode\AdventOfCode\Input\input2.txt")
                 .Split(',')
                 .Select(int.Parse)
                 .ToArray();
-
-            program[1] = 12;
-            program[2] = 2;
-
-            RunProgram(program);
-            _Answer1 = program[0];
-
         }
-        private void RunProgram(int[] program)
-        {
-            int position1 = 0;
-            int position2 = 0;
-            int position3 = 0;
 
-            for (var i = 0; i < program.Length; i += 4)
-            {
-                switch (program[i])
-                {
-                    case 1:
-                        position1 = program[i + 1];
-                        position2 = program[i + 2];
-                        position3 = program[i + 3];
-                        program[position3] = program[position1] + program[position2];
-                        break;
-                    case 2:
-                        position1 = program[i + 1];
-                        position2 = program[i + 2];
-                        position3 = program[i + 3];
-                        program[position3] = program[position1] * program[position2];
-                        break;
-                    case 99:
-                        return;
-                    default:
-                        break;
-                }
-            }
-        }
         public override string ToString()
         {
             return $"Day 2 => Answer A:{_Answer1}, Answer B:{_Answer2}";

f17b00efb64e367b00a190fce86f87cb6a68337e
george xiros <geo.xiros@gmail.com>
2019-12-03 08:15:31 +0200

    first commit

---
+++ AdventOfCode/Day1.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+
+namespace AdventOfCode
+{
+    class Day1
+    {
+        private long _total1;
+        private long _total2;
+
+        public Day1()
+        {
+            using (var file = File.OpenRead(@"C:\Users\George\source\repos\AdventOfCode\AdventOfCode\Input\input1.txt"))
+            {
+                using (var reader = new StreamReader(file))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var temp = (long.Parse(reader.ReadLine()) / 3) - 2;
+
+                        _total1 += temp;
+                        
+                        while (temp > 0)
+                        {
+                            _total2 += temp;
+                            temp = (temp / 3) - 2;
+                        }
+                    }
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return $"Day 1 => Answer A:{_total1}, Answer B:{_total2}";
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Day2());
+
+            Console.ReadKey();
+        }
+    }
+    class Day2
+    {
+        private long _Answer1;
+        private long _Answer2;
+
+        public Day2()
+        {
+            var program = File
+                .ReadAllText(@"C:\Users\George\source\repos\AdventOfCode\AdventOfCode\Input\input2.txt")
+                .Split(',')
+                .Select(int.Parse)
+                .ToArray();
+
+            program[1] = 12;
+            program[2] = 2;
+
+            RunProgram(program);
+            _Answer1 = program[0];
+
+        }
+        private void RunProgram(int[] program)
+        {
+            int position1 = 0;
+            int position2 = 0;
+            int position3 = 0;
+
+            for (var i = 0; i < program.Length; i += 4)
+            {
+                switch (program[i])
+                {
+                    case 1:
+                        position1 = program[i + 1];
+                        position2 = program[i + 2];
+                        position3 = program[i + 3];
+                        program[position3] = program[position1] + program[position2];
+                        break;
+                    case 2:
+                        position1 = program[i + 1];
+                        position2 = program[i + 2];
+                        position3 = program[i + 3];
+                        program[position3] = program[position1] * program[position2];
+                        break;
+                    case 99:
+                        return;
+                    default:
+                        break;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return $"Day 2 => Answer A:{_Answer1}, Answer B:{_Answer2}";
+        }
+    }
+}

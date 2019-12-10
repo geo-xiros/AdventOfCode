c3a3e8092b276247f09cf0e130cf25f322992e9c
george xiros <geo.xiros@gmail.com>
2019-12-10 21:35:16 +0200

    Day 8 answer 1

---
+++ AdventOfCode/Day6.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace AdventOfCode
+{
+    public class Day6
+    {
+        private int _answer1;
+        private int _answer2;
+        private Dictionary<string, Object> _orbitsMap;
+
+        public Day6()
+        {
+
+            var orbits = ReadFile();// "COM)B,K)L,C)D,B)C,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)YOU,I)SAN".Split(',').AsEnumerable();
+            _answer1 = GetTotalOrbits(orbits);
+
+        }
+        private int GetTotalOrbits(IEnumerable<string> orbits)
+        {
+            _orbitsMap = new Dictionary<string, Object>();
+
+            foreach (var orbit in orbits)
+            {
+                var objectOrbit = orbit.Split(')');
+
+                if (!_orbitsMap.TryGetValue(objectOrbit[0], out var centerObject))
+                {
+                    centerObject = new Object(objectOrbit[0]);
+                    _orbitsMap.Add(objectOrbit[0], centerObject);
+                }
+
+                if (!_orbitsMap.TryGetValue(objectOrbit[1], out var orbitingObject))
+                {
+                    orbitingObject = new Object(objectOrbit[1], centerObject);
+                    _orbitsMap.Add(objectOrbit[1], orbitingObject);
+                }
+                else if (orbitingObject.OrbitsAroundObject == null)
+                {
+                    orbitingObject.OrbitAround(centerObject);
+                }
+            }
+
+            return _orbitsMap.Values.Select(o => o.TotalOrbits).Sum();
+        }
+        private IEnumerable<string> ReadFile()
+        {
+            using (var file = File.OpenRead("..\\..\\input6.txt"))
+            {
+                using (var reader = new StreamReader(file))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        yield return reader.ReadLine();
+                    }
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return $"{this.GetType().Name} => Answer A:{_answer1}, Answer B:{_answer2}";
+        }
+
+        private class Object
+        {
+            public readonly string Name;
+            public Object OrbitsAroundObject { get; private set; }
+
+            public Object(string name)
+            {
+                Name = name;
+            }
+
+            public Object(string name, Object orbitsAroundObject) : this(name)
+            {
+                OrbitsAroundObject = orbitsAroundObject;
+            }
+            public void OrbitAround(Object obj)
+            {
+                OrbitsAroundObject = obj;
+            }
+            public int TotalOrbits => (OrbitsAroundObject?.TotalOrbits + 1) ?? 0;
+        }
+    }
+
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
-using System.Linq;
-using System.IO;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode
 {
@@ -14,94 +14,50 @@ namespace AdventOfCode
         static void Main(string[] args)
         {
             //Console.WriteLine(new Day1());
-            Console.WriteLine(new Day2());
+            //Console.WriteLine(new Day2());
             //Console.WriteLine(new Day3());
             //Console.WriteLine(new Day4());
-            Console.WriteLine(new Day5());
+            //Console.WriteLine(new Day5());
             //Console.WriteLine(new Day6());
-            
+            Console.WriteLine(new Day8());
+            //var x = new Day8();
             Console.ReadKey();
         }
 
     }
-    public class Day6
+    public class Day8
     {
-        private int _answer1;
-        private int _answer2;
-        private Dictionary<string, Object> _orbitsMap;
-
-        public Day6()
+        private long _answer1;
+        private long _answer2;
+        public Day8()
         {
+            var image = File.ReadAllText("..\\..\\input8.txt");
 
-            var orbits = ReadFile();// "COM)B,K)L,C)D,B)C,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)YOU,I)SAN".Split(',').AsEnumerable();
-            GenerateOrbitsMap(orbits);
-
+            _answer1 = ImageLayers(image, 25, 6)
+               .OrderBy(layer => CountOf(layer, '0'))
+               .Take(1)
+               .Select(layer => CountOf(layer, '1') * CountOf(layer, '2'))
+               .FirstOrDefault();
         }
-        private void GenerateOrbitsMap(IEnumerable<string> orbits)
+        
+        private int CountOf(string str, char charDigit)
         {
-            _orbitsMap = new Dictionary<string, Object>();
-
-            foreach (var orbit in orbits)
-            {
-                var objectOrbit = orbit.Split(')');
-
-                if (!_orbitsMap.TryGetValue(objectOrbit[0], out var centerObject))
-                {
-                    centerObject = new Object(objectOrbit[0]);
-                    _orbitsMap.Add(objectOrbit[0], centerObject);
-                }
-
-                if (!_orbitsMap.TryGetValue(objectOrbit[1], out var orbitingObject))
-                {
-                    orbitingObject = new Object(objectOrbit[1], centerObject);
-                    _orbitsMap.Add(objectOrbit[1], orbitingObject);
-                }
-                else if (orbitingObject.OrbitsAroundObject == null)
-                {
-                    orbitingObject.OrbitAround(centerObject);
-                }
-            }
-
-            _answer1 = _orbitsMap.Values.Select(o => o.TotalOrbits).Sum();
+            return str.Count(c => c.Equals(charDigit));
         }
-        private IEnumerable<string> ReadFile()
+
+        private IEnumerable<string> ImageLayers(string image, int imageWidth, int imageHeight)
         {
-            using (var file = File.OpenRead("..\\..\\input6.txt"))
+            int layerSize = imageWidth * imageHeight;
+
+            for (var index = 0; index < image.Length - 1; index += layerSize)
             {
-                using (var reader = new StreamReader(file))
-                {
-                    while (!reader.EndOfStream)
-                    {
-                        yield return reader.ReadLine();
-                    }
-                }
+                yield return image.Substring(index, layerSize);
             }
         }
+        
         public override string ToString()
         {
             return $"{this.GetType().Name} => Answer A:{_answer1}, Answer B:{_answer2}";
         }
-
-        private class Object
-        {
-            public readonly string Name;
-            public Object OrbitsAroundObject { get; private set; }
-
-            public Object(string name)
-            {
-                Name = name;
-            }
-
-            public Object(string name, Object orbitsAroundObject) : this(name)
-            {
-                OrbitsAroundObject = orbitsAroundObject;
-            }
-            public void OrbitAround(Object obj)
-            {
-                OrbitsAroundObject = obj;
-            }
-            public int TotalOrbits => (OrbitsAroundObject?.TotalOrbits + 1) ?? 0;
-        }
     }
-
 }

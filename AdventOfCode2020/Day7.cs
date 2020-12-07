6ff6cefaf361c6e092df0dca29c775d42a707313
george xiros <geo.xiros@gmail.com>
2020-12-07 21:30:43 +0200

    better regex use

--- AdventOfCode2020/Day7.cs
+++ AdventOfCode2020/Day7.cs
@@ -11,34 +11,31 @@ namespace AdventOfCode2020
 
         public Day7() : base(7)
         {
-            MakeInput();
+            PrepareInput();
         }
 
         protected override int GetAnswer1()
         {
             return bags.Values
-                .Count(b => b.CanContain("shiny gold bag")) 
+                .Count(b => b.CanContain("shiny gold"))
                 - 1; // 'shiny gold bag' rule should not count
         }
 
         protected override int GetAnswer2()
         {
-            return bags["shiny gold bag"].Quantity;
+            return bags["shiny gold"].Quantity;
         }
 
-        private void MakeInput()
+        private void PrepareInput()
         {
             input.ToList().ForEach(line =>
             {
-                var splitLine = line.Split(" contain ");
-                var quantities = splitLine[1].Split(',').Select(s => GetQuantity(s) ?? "0").Select(s => string.IsNullOrEmpty(s) ? 0 : int.Parse(s)).ToList();
-                var bagColorCodes = splitLine[1].Split(',').Select(s => GetColorCode(s)).ToList();
-                var bagColorCode = GetColorCode(splitLine[0]);
+                var bagRule = Regex.Match(line, @"(\w+\s\w+)\sbags\scontain").Groups[1].Value;
+                var bag = GetOrAdd(bagRule);
 
-                var bag = GetOrAdd(bagColorCode);
-
-                bagColorCodes
-                    .Zip(quantities, (colorCode, quantity) => new ContainedBag(GetOrAdd(colorCode), quantity))
+                Regex
+                    .Matches(line, @"(?<quantity>[0-9]+)\s(?<code>\w+\s\w+)\sbags?")
+                    .Select(m => new ContainedBag(GetOrAdd(m.Groups["code"].Value), int.Parse(m.Groups["quantity"].Value)))
                     .ToList()
                     .ForEach(cb => bag.Add(cb));
             });
@@ -46,17 +43,16 @@ namespace AdventOfCode2020
 
         private Bag GetOrAdd(string bagColorCode)
         {
-            if (!bags.TryGetValue(bagColorCode, out Bag bag))
+            if (bags.TryGetValue(bagColorCode, out Bag bag))
             {
-                bag = new Bag(bagColorCode);
-                bags[bagColorCode] = bag;
+                return bag;
             }
+
+            bag = new Bag(bagColorCode);
+            bags[bagColorCode] = bag;
+
             return bag;
         }
-
-        private string GetColorCode(string line) => Regex.Match(line, @"(?<color>\w*\s\w*\sbag)").Value;
-
-        private string GetQuantity(string line) => Regex.Match(line, @"\s*[0-9]*").Value.Trim();
 
         private class Bag
         {
